f1741b343922bef8adafa60694c80b6b100e00a1
adao junior <adaojunior2701@gmail.com>
2023-10-18 19:26:14 -0300

    foi adicionado o arquivo tabelas.html

---
+++ desafio/desafio.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur aliquam, voluptatum pariatur necessitatibus molestiae ex assumenda illo natus et porro quia minima iure maiores dolores aspernatur impedit earum doloremque. Eum!
+</body>/
+
+</html>
